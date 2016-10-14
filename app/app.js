1f58c038fa9c873c9426a14ecf5388c19e5a7ff7
AlexMarvelo <mironenkoalexey@gmail.com>
2016-10-14 11:43:23 +0300

    add username login

--- app/app.js
+++ app/app.js
@@ -1,12 +1,70 @@
 var socket = io();
-var form = $('#form-newmessage'),
-    input = form.find('.form-input')[0];
 
-$(form).submit(function(){
-  socket.emit('chat message', input.value);
-  input.value = '';
-  return false;
-});
-socket.on('chat message', function(msg){
-  console.warn(msg);
+$(document).ready(function() {
+  const form = $('#form-newmessage');
+  const input = form.find('.form-input')[0];
+  const entryForm = $('#entry-form');
+  const entryInput = entryForm.find('.entry-input')[0];
+  const messageList = $('#messages-list');
+  entryInput.focus();
+
+  let currentUser;
+
+  $(entryForm).submit((event) => {
+    event.preventDefault();
+    socket.emit('user login', {
+      name: entryInput.value,
+      time: new Date()
+    });
+    entryForm.remove();
+  });
+
+  $(form).submit((event) => {
+    event.preventDefault();
+    if (!currentUser) {
+      input.value = '';
+      return;
+    }
+    let msg = {
+      text: input.value,
+      user: currentUser.name,
+      time: new Date()
+    };
+    messageList.append(getMessageBlock(msg));
+    socket.emit('chat message', msg);
+    input.value = '';
+  });
+
+  socket.on('user logined', user => {
+    currentUser = user;
+    console.log(currentUser);
+  });
+
+  socket.on('chat message', msg => {
+    if (msg.user == currentUser.name) return;
+    messageList.append(getMessageBlock(msg));
+  });
+
+  function getMessageBlock(msg) {
+    console.dir(msg);
+    var getTimeString = function(time) {
+      time = new Date(time);
+      const now = new Date();
+      const isToday = time.getDate() == now.getDate() && time.getMonth() == now.getMonth();
+      const dateStr = isToday ? 'Today' : `${time.getDate()}.${time.getMonth()+1}`;
+      const timeStr = `${time.getHours() < 10 ? 0 : ''}${time.getHours()}:${time.getMinutes() < 10 ? 0 : ''}${time.getMinutes()}`;
+      return `${dateStr} ${timeStr}`;
+    };
+    return `
+    <li class="message message--${msg.user == currentUser.name ? 'inside' : 'outside'}">
+      <div class="message-imgContainer">
+        <img src="assets/img/user-avatar.png" alt="${msg.user}" />
+      </div>
+      <div class="message-content">
+        <span class="message-content-user">${msg.user}</span>
+        <p class="message-content-text">${msg.text}</p>
+        <span class="message-content-time">${getTimeString(msg.time)}</span>
+      </div>
+    </li>`;
+  }
 });
--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-var express = require('express'),
-    app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+io.on('connection', socket => {
+  // console.log('a user connected');
+  // socket.on('disconnect', () => {
+  //   console.log('user disconnected');
+  // });
 
   socket.broadcast.emit('hi');
-  socket.on('chat message', function(msg){
+
+  socket.on('chat message', msg => {
     io.emit('chat message', msg);
+  });
+
+  socket.on('user login', user => {
+    io.emit('user logined', user);
   });
 });
 
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/assets', express.static(__dirname + '/assets'));
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
